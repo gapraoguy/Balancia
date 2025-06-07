fdce1eec7691a79c6886fbdd99c919b685fa0efa
Sen Nagasaka <0307.sen@gmail.com>
2025-06-07 18:50:11 +0900

    Initial Commit

---
+++ Balancia/BalanciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanciaApp.swift
+//  Balancia
+//
+//  Created by Sen Nagasaka on 2025/06/07.
+//
+
+import SwiftUI
+
+@main
+struct BalanciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
