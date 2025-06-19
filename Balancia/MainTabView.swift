5c74b4b07558e6464b5dbc1ba92adb25be99f706
Sen Nagasaka <0307.sen@gmail.com>
2025-06-19 12:20:10 +0900

    Entryの編集に関する不具合を修正

--- Balancia/MainTabView.swift
+++ Balancia/MainTabView.swift
@@ -4,39 +4,25 @@ struct MainTabView: View {
     @StateObject private var entryFormViewModel = EntryFormViewModel()
     @StateObject private var summaryViewModel = SummaryViewModel()
     @StateObject private var entryListViewModel = EntryListViewModel()
-    @State private var selectedTab: Tab = .entryForm
-    
-    enum Tab {
-        case entryForm, entryList, summary, settings
-    }
     
     var body: some View {
         
-        TabView(selection: $selectedTab) {
-            EntryFormView()
-                .tabItem {
-                    Label("記録", systemImage: "plus.circle")
-                }
-                .tag(Tab.entryForm)
-            
-            EntryListView(selectedTab: $selectedTab)
+        TabView() {
+            EntryListView()
                 .tabItem {
                     Label("履歴", systemImage: "list.bullet")
                 }
-                .tag(Tab.entryList)
 
             SummaryView()
                 .tabItem {
                     Label("集計", systemImage: "chart.bar.fill")
                 }
-                .tag(Tab.summary)
             
             
             SettingsView()
                 .tabItem {
                     Label("設定", systemImage: "gear")
                 }
-                .tag(Tab.settings)
         }
         .environmentObject(entryFormViewModel)
         .environmentObject(summaryViewModel)
